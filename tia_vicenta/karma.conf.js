e0bc73018009cf0cd3505ddfd93bb7584ca2adb3
alex.nunez <alex.nunez@ozona.es>
2023-03-27 02:03:15 +0200

    test if works with browser chrome and some steps for yml

--- tia_vicenta/karma.conf.js
+++ tia_vicenta/karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
